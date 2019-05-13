8fa3b803161a22c95a823c38c2c82c642436cbc5
MacroUniverse <270174408@qq.com>
2019-05-13 15:24:38 -0500

    app example: TODO

---
+++ JavaScriptTest/test.js
@@ -0,0 +1,9 @@
+var cars = ["littleshi.cn", "Hongyu", "MacroUniverse"];
+var text = "";
+var i;
+for (i = 0; i < cars.length; ++i) {
+  text += cars[i] + "<br>";
+}
+document.write(text);
+document.write("<br><br>");
+document.write(text);
